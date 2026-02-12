2799b90a303e36353d75149750716ac25ce2b599
ihdayatul <140098146+ihdayatul@users.noreply.github.com>
2026-02-12 13:31:40 +0700

    132

--- Assets/Script/LevelMenu.cs
+++ Assets/Script/LevelMenu.cs
@@ -4,24 +4,24 @@ using UnityEngine.UI;
 
 public class LevelMenu : MonoBehaviour
 {
-    //public Button[] buttons;
+    public Button[] buttons;
 
-    //private void Awake()
-    //{
-    //    int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
-    //    for (int i = 0; i < buttons.Length; i++)
-    //    {
-    //        buttons[i].interactable = false;
-    //    }
-    //    for (int i = 0;i < unlockedLevel; i++)
-    //    {  
-    //        buttons[i].interactable = true;
-    //    }
-    //}
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        //for (int i = 0; i < unlockedLevel; i++)
+        //{
+        //    buttons[i].interactable = true;
+        //}
+    }
 
-    //public void OpenLevel(int levelId)
-    //{
-    //    string LevelName = "Level " + levelId;
-    //    SceneManager.LoadScene (LevelName);
-    //}
+    public void OpenLevel(int levelId)
+    {
+        string LevelName = "Level " + levelId;
+        SceneManager.LoadScene(LevelName);
+    }
 }
